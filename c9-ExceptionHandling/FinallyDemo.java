3039563655a6adf4f6280b14a3a6c7a505c984a4
iramoz95 <ian.ramoz95@gmail.com>
2022-10-17 17:53:32 -0500

    Using Finally

---
+++ c9-ExceptionHandling/FinallyDemo.java
@@ -0,0 +1,36 @@
+// Use finally
+class UseFinally {
+    public static void genException(int what) {
+        int t;
+        int[] nums = new int[2];
+        System.out.println("Receiving " + what);
+        try {
+            switch (what) {
+                case 0:
+                    t = 10 / what; // generate dev-by-zero error
+                    break;
+                case 1:
+                    nums[4] = 4; // generate array index error.
+                    break;
+                case 2:
+                    return; // return from try block
+            }
+        } catch (ArithmeticException exc) {
+            System.out.println("Can't divide by Zero!");
+            return;
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("No matching element found");
+        } finally {
+            System.out.println("Leaving try.");
+        }
+    }
+}
+
+class FinallyDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            UseFinally.genException(i);
+            System.out.println();
+        }
+    }
+}
