8512fb45ba3f37687a34ea673b7c9f643f84a965
iramoz95 <ian.ramoz95@gmail.com>
2022-10-24 18:12:22 -0500

    Type wrappers

---
+++ c10-UsingIO/AvgNums.java
@@ -0,0 +1,38 @@
+
+// This program averages a list of numbers entered by the user.
+import java.io.*;
+
+class AvgNums {
+    public static void main(String[] args) throws IOException {
+        // create a BufferedReader using System.in
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, "utf-8"));
+        String str;
+        int n;
+        double sum = 0.0;
+        double avg, t;
+
+        System.out.println("How many numbers will you enter: ");
+        str = br.readLine();
+        try {
+            n = Integer.parseInt(str);
+        } catch (NumberFormatException exc) {
+            System.out.println("Invalid Format");
+            n = 0;
+        }
+
+        System.out.println("Enter " + n + " values.");
+        for (int i = 0; i < n; i++) {
+            System.out.print(": ");
+            str = br.readLine();
+            try {
+                t = Double.parseDouble(str);
+            } catch (NumberFormatException exc) {
+                System.out.println("Invalid Format");
+                t = 0.0;
+            }
+            sum += t;
+        }
+        avg = sum / n;
+        System.out.println("Average is " + avg);
+    }
+}
