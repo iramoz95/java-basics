a32498cbd0c4bffb14fb4a440602892086b67027
iramoz95 <ian.ramoz95@gmail.com>
2022-10-24 08:44:14 -0500

    Random Access Files

--- c10-UsingIO/CompFiles.java
+++ c10-UsingIO/CompFiles.java
@@ -10,7 +10,7 @@ import java.io.*;
 
 class CompFiles {
     public static void main(String[] args) {
-        int i = 0, j = 0;
+        int i = 0, j = 0, line = 1;
         // First make sure that both files have been specified
         if (args.length != 2) {
             System.out.println("Usage: CompFiles f1 f2");
@@ -23,12 +23,14 @@ class CompFiles {
             do {
                 i = f1.read();
                 j = f2.read();
+                if (i == '\n' || j == '\n')
+                    line += 1;
                 if (i != j)
                     break;
             } while (i != -1 && j != -1);
 
             if (i != j)
-                System.out.println("Files differ.");
+                System.out.println("Files differ, at line " + line);
             else
                 System.out.println("Files are the same");
 
